2b065b245f37797c0231f945d1836b8a60a53c76
ntheresa <ntheresa@umich.edu>
2018-01-31 14:53:32 -0500

    Added list of answers

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,11 @@
+from random import randrange
 question = "What is your question?"
 print(question)
+
+lst_of_answers =  ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", " You may rely on it",
+"As I see it, yes", "Most likely", "Outlook good", "Yes", " Signs point to yes", "Reply hazy try again",
+"Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it",
+"My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
+
+random_answer = randrange(0,len(lst_of_answers))
+print(lst_of_answers[random_answer])
