ed17e1c3a865114bd139675c4669e08b3b4a2ec4
William Chen <chenwil@umich.edu>
2018-01-31 14:57:03 -0500

    End in question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,7 @@
-question = "What is your question?"
+question = input("What is your question?")
 print(question)
+
+while question != "quit":
+    print(question)
+    if question[-1] != '?':
+        print("I'm sorry, I can only answer questions")
